145e04a071bec34dfdd9ce0cb79540222e0611bf
小形克宏 <ogwata@vivliostyle.org>
2025-01-17 00:25:44 +0900

    諸ファイル追加

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'jagat-20250117', // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
+  title: 'Web技術によるページ組版とVivliostyle ', // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
   author: '小形克宏 <ogwata@vivliostyle.org>', // default to `author` in `package.json` or undefined.
-  // language: 'ja', // default to undefined.
+language: 'ja', // default to undefined.
   // size: 'A4', // paper size.
-  theme: '@vivliostyle/theme-slide@^0.5.1', // .css or local dir or npm package. default to undefined.
+  theme: 'themes/theme.css', // .css or local dir or npm package. default to undefined.
   entry: [
     'manuscript.md', // `title` is automatically guessed from the file (frontmatter > first heading).
     // {
@@ -14,13 +14,13 @@ module.exports = {
     // 'glossary.html', // html can be passed.
   ], // `entry` can be `string` or `object` if there's only single markdown file.
   // entryContext: './manuscripts', // default to '.' (relative to `vivliostyle.config.js`).
-  // output: [ // path to generate draft file(s). default to '{title}.pdf'
-  //   './output.pdf', // the output format will be inferred from the name.
-  //   {
-  //     path: './book',
-  //     format: 'webpub',
-  //   },
-  // ],
+  output: [
+    'jagat-20250117.pdf',
+    {
+      path: './book',
+      format: 'webpub',
+    },
+  ],
   // workspaceDir: '.vivliostyle', // directory which is saved intermediate files.
   // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
   // cover: './cover.png', // cover image. default to undefined.
